daf7bf13c740d14e1f113da87660142402d44c37
Filbert Daniel <filbertdan67@gmail.com>
2023-11-20 18:58:08 +0300

    Update student.rb
    Co-authored-by: Shahadat Hossain <shahadat3669@gmail.com>

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
-    super(age: age, parent_permission: parent_permission, name: name)
+    super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
   end
 
