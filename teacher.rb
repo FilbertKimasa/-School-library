318b41b256305dda52142014c12fba1c56e35b5f
Filbert Daniel <filbertdan67@gmail.com>
2023-11-20 18:57:38 +0300

    Update teacher.rb
    Co-authored-by: Shahadat Hossain <shahadat3669@gmail.com>

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(specialization, age, parent_permission: true, name: 'Unknown')
-    super(age: age, parent_permission: parent_permission, name: name)
+    super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
 
