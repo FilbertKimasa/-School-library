c31bb4acb79f9ccfedd2f04b97234ae3fa9ab17c
FilbertKimasa <filbertdan67@gmail.com>
2023-11-23 14:31:21 +0300

    Add comments to the main.rb file

--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ require_relative 'capitalize_decorator'
 require_relative 'trim_decorator'
 
 person = Person.new(22, name: 'maximilianus')
-person.correct_name
+puts person.correct_name
+
+# Appliying decorators
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
