00d6d2f71990b5da014e09d8b5efde7359ed3db2
FilbertKimasa <filbertdan67@gmail.com>
2023-11-23 14:01:32 +0300

    Implement student class

--- student.rb
+++ student.rb
@@ -1,12 +1,20 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
+    classroom&.students&.push(self)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
+
+  def set_classroom(classroom)
+    @classroom&.students&.delete(self) 
+    @classroom = classroom
+    classroom&.students&.push(self) 
 end
