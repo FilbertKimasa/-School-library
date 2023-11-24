2eaddf8a592879b1e4cc55ccf3e43963ecb11da3
FilbertKimasa <filbertdan67@gmail.com>
2023-11-24 17:08:40 +0300

    Modify setters and getters

--- student.rb
+++ student.rb
@@ -1,13 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
-    # classroom&.students&.push(self)
-    classroom&.add_student(self)
   end
 
   def class_name
@@ -19,8 +17,7 @@ class Student < Person
   end
 
   def add_to_classroom(new_classroom)
-    @classroom&.students&.delete(self)
     @classroom = new_classroom
-    new_classroom&.students&.push(self)
   end
+
 end
