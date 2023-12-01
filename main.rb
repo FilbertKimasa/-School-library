3e13913ae2caf98f749befec6ea9d0b785b01192
FilbertKimasa <filbertdan67@gmail.com>
2023-12-01 07:38:43 +0300

    Modify output

--- main.rb
+++ main.rb
@@ -16,22 +16,20 @@ puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
 
-# classroom_a = Classroom.new('Class A')
-# person = Student.new(classroom_a, 17, name: 'John Doe')
-# book = Book.new('Ruby Programming', 'John Smith')
+classroom_a = Classroom.new('Class A')
+person = Student.new(classroom_a, 17, name: 'John Doe')
+book = Book.new('Ruby Programming', 'John Smith')
 
-# # Create a rental and associate it with the person and book
-# rental = Rental.new('2023-11-21', book, person)
 
-# # Output information
-# puts "Person: #{person.name}, Age: #{person.age}, Class: #{person.class_name}"
-# puts "Book: #{book.title}, Author: #{book.author}"
+# Output information
+puts "Person: #{person.name}, Age: #{person.age}, Class: #{person.class_name}"
+puts "Book: #{book.title}, Author: #{book.author}"
 
 # Creating instances of Person and Book
 person = Person.new(25, name: 'John')
 book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
 
-# Creating a rental
+# Create a rental and associate it with the person and book
 date = Date.new(2023, 12, 15)
 rental = Rental.new( date, book, person)
 
